cc5dde1a6a99f6b78277c9344c5b70ca8f89bf47
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-11-13 08:38:59 -0300

    fix: corrigindo função de backup e restore

--- App/Controller/BackupController.php
+++ App/Controller/BackupController.php
@@ -6,90 +6,32 @@ use App\Controller\Controller;
 
 class BackupController extends Controller 
 {
-    public static function backup()
+    public static function Backup()
     {
-        $host = 'localhost:3307';
-        $usuario = 'root';
-        $senha = 'etecjau';
-        $banco = 'db_petshop';
-        $caminhoBackup = 'E:/Backup';
+        $command = '"C:/Program Files/MySQL/MySQL Server 8.0/bin/mysqldump" -uroot -petecjau -P3307 -hlocalhost db_petshop > D:/backup/file.sql';
+        exec($command, $output, $exit_code);
 
-        $resultadoBackup = self::realizarBackup($host, $usuario, $senha, $banco, $caminhoBackup);
-
-        if ($resultadoBackup !== false) {
-            $message = "Backup realizado com sucesso. Caminho do arquivo: {$resultadoBackup}";
+        if ($exit_code === 0) {
+            /*echo "<script>alert('Backup realizado com sucesso.');</script>";*/
+            $message = 'Backup realizado com sucesso.';
         } else {
             $message = 'Erro ao realizar o backup.';
         }
 
-        header('Location: /home?message=' . urlencode($message));
+        header('Location: /home?message=' . urlencode($message));   
     }
 
     public static function restoreBackup()
     {
-        $host = 'localhost:3307';
-        $usuario = 'root';
-        $senha = 'etecjau';
-        $banco = 'db_petshop';
-        $caminhoBackup = 'D:/backup';
-        $nomeArquivoBackup = 'file.sql';
+        $command = '"C:/Program Files/MySQL/MySQL Server 8.0/bin/mysql" -uroot -petecjau -P3307 -hlocalhost db_petshop < D:/backup/file.sql';
+        exec($command, $output, $exit_code);
 
-        $resultadoRestore = self::restaurarBackup($host, $usuario, $senha, $banco, $caminhoBackup, $nomeArquivoBackup);
-
-        if ($resultadoRestore !== false) {
+        if ($exit_code === 0) {
             $message = 'Restore realizado com sucesso.';
         } else {
             $message = 'Erro ao realizar o restore.';
         }
 
         header('Location: /home?message=' . urlencode($message));
-    }
-
-    private static function realizarBackup($host, $usuario, $senha, $banco, $caminhoBackup)
-    {
-        try {
-            // Gera o nome do arquivo de backup com base na data e hora
-            $dataHoraAtual = date('Ymd_His');
-            $nomeArquivo = "backup_{$banco}_{$dataHoraAtual}.sql";
-
-            // Caminho completo para o arquivo de backup
-            $caminhoCompleto = $caminhoBackup . '/' . $nomeArquivo;
-
-            // Comando mysqldump
-            $comando = "\"C:/Program Files/MySQL/MySQL Server 8.0/bin/mysqldump\" -h{$host} -u{$usuario} -p{$senha} {$banco} > {$caminhoCompleto}";
-
-            // Executa o comando
-            exec($comando, $output, $exitCode);
-
-            // Verifica se o backup foi criado com sucesso
-            if ($exitCode === 0) {
-                return $caminhoCompleto;
-            } else {
-                return false;
-            }
-        } catch (\Exception $e) {
-            // Lida com qualquer exceção que ocorra durante o backup
-            return false;
-        }
-    }
-
-    private static function restaurarBackup($host, $usuario, $senha, $banco, $caminhoBackup, $nomeArquivoBackup)
-    {
-        try {
-            // Caminho completo para o arquivo de backup
-            $caminhoCompleto = $caminhoBackup . '/' . $nomeArquivoBackup;
-
-            // Comando mysql para restaurar o backup
-            $comando = "\"C:/Program Files/MySQL/MySQL Server 8.0/bin/mysql\" -h{$host} -u{$usuario} -p{$senha} {$banco} < {$caminhoCompleto}";
-
-            // Executa o comando
-            exec($comando, $output, $exitCode);
-
-            // Verifica se o restore foi concluído com sucesso
-            return $exitCode === 0;
-        } catch (\Exception $e) {
-            // Lida com qualquer exceção que ocorra durante o restore
-            return false;
-        }
     }
 }
